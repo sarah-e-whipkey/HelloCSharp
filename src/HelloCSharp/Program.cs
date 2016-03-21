02d63d4d1fe2dcaf33d786d3cc953f2895d279e3
Sarah Whipkey <sarah.e.whipkey@gmail.com>
2016-03-21 12:07:09 -0500

    csharp practice

---
+++ src/HelloCSharp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HelloCSharp
+{
+    public class Program
+    {
+
+        /// <summary>
+        /// The entry point to my program
+        /// </summary>
+        /// <param name="args">the command line arguments</param>
+        public static void Main(string[] args) //Main method has to be static; this is where C# starts running
+        {
+
+            //DateTime dog;
+            //if (DateTime.TryParse(Console.ReadLine(), out dog))
+            //{
+            //    Console.WriteLine(dog.ToString("D"));
+            //}
+            //else {
+            //    Console.WriteLine("That wasn't a date");
+            //}
+
+            //Console.ReadLine();
+
+
+
+            //var cat = DateTime.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine(String.Format("{0:f}", cat));
+            //Console.ReadLine();
+
+
+            //var word = @"\w+"; //absolute verbatim operator
+
+
+            // Enter email addess
+            Console.Write("Email Address:");
+            var email = Console.ReadLine();
+
+            // Validate email
+            if (!Regex.IsMatch(email, @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"))
+            {
+                Console.WriteLine("Invalid email address!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Email validated");
+                Console.ReadLine();
+
+
+            }
+        }
+
+        
+        
+    }
+}
---
+++ src/HelloCSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HelloCSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloCSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("179f07fb-098c-45d2-991d-2a16480b9d99")]
